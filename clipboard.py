ea769afee70b546d08d85f5ad38c5f59f7ea51ae
Anthony Mayer <anthonym@yelp.com>
2013-06-18 12:06:07 -0700

    Adding command to copy testify command for current file.
    Also, some minor code cleanup.

--- clipboard.py
+++ clipboard.py
@@ -2,8 +2,13 @@ import sublime, sublime_plugin
 
 class PathToClipboardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sublime.set_clipboard(
-			sublime.active_window().active_view().file_name().replace(
-				sublime.active_window().folders()[0] + '/', ''
-			)
-		)
+		sublime.set_clipboard(root_file_path())
+
+class TestifyForCurrentPathToClipboardCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sublime.set_clipboard('testify ' + root_file_path().replace('.py', '').replace('/', '.'))
+
+def root_file_path():
+	return sublime.active_window().active_view().file_name().replace(
+		sublime.active_window().folders()[0] + '/', ''
+	)
