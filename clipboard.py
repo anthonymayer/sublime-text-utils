13af815ca9fdaa95a4d5bca67313cb536d4a0bc2
Anthony Mayer <anthonym@yelp.com>
2013-12-20 15:20:12 +0100

    dotted_path() now accounts for .tmpl as well as .py. The dotted path command now appends any selected text to the path.

--- clipboard.py
+++ clipboard.py
@@ -1,3 +1,5 @@
+import re
+
 import sublime, sublime_plugin
 
 class PathToClipboardCommand(sublime_plugin.TextCommand):
@@ -6,23 +8,17 @@ class PathToClipboardCommand(sublime_plugin.TextCommand):
 
 class TestifyForCurrentPathToClipboardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sel = self.view.sel()
-
-		selectedText = ' ' + self.view.substr(sel[0]) if sel else ''
-
-		sublime.set_clipboard('testify -d ' + dotted_path() + selectedText)
+		sublime.set_clipboard('testify -d ' + dotted_path() + get_selected_text(self))
 
 class ImportForCurrentPathToClipboardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sel = self.view.sel()
-
-		selectedText = self.view.substr(sel[0]) if sel else ''
-
-		sublime.set_clipboard('from ' + dotted_path() + ' import ' + selectedText)
+		sublime.set_clipboard('from ' + dotted_path() + ' import ' + get_selected_text(self))
 
 class DottedPathToClipboardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sublime.set_clipboard(dotted_path())
+		selectedText = get_selected_text(self)
+		dottedSelectedText = '.' + selectedText if selectedText else ''
+		sublime.set_clipboard(dotted_path() + dottedSelectedText)
 
 def root_file_path():
 	return sublime.active_window().active_view().file_name().replace(
@@ -30,4 +26,8 @@ def root_file_path():
 	)
 
 def dotted_path():
-	return root_file_path().replace('.py', '').replace('/', '.')
+	return re.sub(r'\.py|\.tmpl', '', root_file_path()).replace('/', '.')
+
+def get_selected_text(sublime_plugin_text_command):
+	sel = sublime_plugin_text_command.view.sel()
+	return sublime_plugin_text_command.view.substr(sel[0]).strip() if sel else ''
