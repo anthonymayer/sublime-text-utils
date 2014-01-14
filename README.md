cff1530dcc9d50f7ed9d00095470dc1c315060d2
Anthony Mayer <anthonym@yelp.com>
2014-01-13 17:17:10 -0800

    Converting tabs to spaces(yay!) and adding new command to copy js-tester command for current js file.

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Utilities for Sublime(right now just copying the file path from the root of the 
 
 Current commands
   - Copy file path from root of current sublime project.
-  - Copy [Testify](https://github.com/Yelp/Testify) command for the current file(based on the root path).
-  - Copy import statement for current file("from dotted.file.name.from.root import ").
+  - Copy [Testify](https://github.com/Yelp/Testify) command for the current file(based on the root path). Highlight test class to run the specific test.
+  - Copy import statement for current file("from dotted.file.name.from.root import "). Highlight something to specifically import it.
   - Copy dotted path for current file.
+  - Copy path for js-tester(Mocha utility) of current js file
 
 Installation
 
--- clipboard.py
+++ clipboard.py
@@ -4,31 +4,45 @@ import re
 import sublime, sublime_plugin
 
 class PathToClipboardCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sublime.set_clipboard(root_file_path())
+    def run(self, edit):
+        sublime.set_clipboard(root_file_path())
 
 class TestifyForCurrentPathToClipboardCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sublime.set_clipboard('testify -d ' + dotted_path() + ' ' + get_selected_text(self))
+    def run(self, edit):
+        sublime.set_clipboard('testify -d ' + dotted_path() + ' ' + get_selected_text(self))
 
 class ImportForCurrentPathToClipboardCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sublime.set_clipboard('from ' + dotted_path() + ' import ' + get_selected_text(self))
+    def run(self, edit):
+        sublime.set_clipboard('from ' + dotted_path() + ' import ' + get_selected_text(self))
 
 class DottedPathToClipboardCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		selectedText = get_selected_text(self)
-		dottedSelectedText = '.' + selectedText if selectedText else ''
-		sublime.set_clipboard(dotted_path() + dottedSelectedText)
+    def run(self, edit):
+        selectedText = get_selected_text(self)
+        dottedSelectedText = '.' + selectedText if selectedText else ''
+        sublime.set_clipboard(dotted_path() + dottedSelectedText)
+
+class JsTestToClipboardCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        file_path = sublime.active_window().active_view().file_name()
+        if not '.js' in file_path:
+            return
+        js_file = open(file_path, 'r')
+        for line in js_file:
+            re_match = re.match('describe\([\'"](.*)[\'"]', line)
+            if re_match:
+                test_name = re_match.group(1)
+                break
+        js_file.close()
+        sublime.set_clipboard('js-tester ' + test_name)
 
 def root_file_path():
-	return sublime.active_window().active_view().file_name().replace(
-		sublime.active_window().folders()[0] + os.path.sep, ''
-	)
+    return sublime.active_window().active_view().file_name().replace(
+        sublime.active_window().folders()[0] + os.path.sep, ''
+    )
 
 def dotted_path():
-	return re.sub(r'\.py|\.tmpl', '', root_file_path()).replace(os.path.sep, '.')
+    return re.sub(r'\.py|\.tmpl', '', root_file_path()).replace(os.path.sep, '.')
 
 def get_selected_text(sublime_plugin_text_command):
-	sel = sublime_plugin_text_command.view.sel()
-	return sublime_plugin_text_command.view.substr(sel[0]).strip() if sel else ''
+    sel = sublime_plugin_text_command.view.sel()
+    return sublime_plugin_text_command.view.substr(sel[0]).strip() if sel else ''
